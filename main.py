ea9a06726ec0a5d7103cb308c8964ca91dada9c2
abubakar <mabubakarbssg@gmail.com>
2022-02-23 01:08:57 -0500

    2nd commit

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ a= int(input("emter the number: "))
 b = int(input("emter the number: "))
 sum=a+b
 print(sum)
+a= int(input("emter the number: "))
+b = int(input("emter the number: "))
+a= int(input("emter the number: "))
+b = int(input("emter the number: "))
